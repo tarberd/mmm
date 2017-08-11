8a46907172a4d07716fa88899fd28ff05019afb4
Bernardo Ferrari <bernardo.mferrari@gmail.com>
2017-08-11 13:35:02 -0300

    mmm router routes with one pin

--- src/mmm.h
+++ src/mmm.h
@@ -4,61 +4,63 @@
 #include <algorithm>
 #include <numeric>
 #include <iterator>
+
 #include <lemon/list_graph.h>
+#include <ophidian/geometry/Models.h>
+#include <iostream>
 
 using lemon::ListDigraph;
-using std::pair;
+using ophidian::geometry::Point;
 
 namespace clk_router {
-    template<class RandomIt, class Pair>
-    Pair region_mean(RandomIt begin, RandomIt end){
-        auto mean = std::accumulate(begin,end, Pair{0,0},
-                [](const Pair & a, const Pair & b){
-                    return Pair{a.first + b.first, a.second + b.second};
+    template<class RandomIt>
+    Point region_mean(RandomIt begin, RandomIt end){
+        auto mean = std::accumulate(begin,end, Point{0,0},
+                [](const Point & a, const Point & b) -> Point {
+                    return Point{a.x() + b.x(), a.y() + b.y()};
                 });
-        return Pair{mean.first / std::distance(begin, end), mean.second / std::distance(begin,end)}; 
+        return Point{mean.x() / std::distance(begin, end), mean.y() / std::distance(begin,end)}; 
     }
 
-    template <typename T>
-    static const std::function< bool(pair<T,T>, Pair)> first_less_then = [](const Pair & a, const Pair & b){
-        return a.first < b.first;
+    template<bool inverted = false>
+    struct first_less_then{
+        const bool operator () (const Point &a, const Point &b) const {
+            return a.x() < b.x();
+        }; 
     };
 
-    template <typename T>
-    static const std::function< bool(pair<T,T>, pair<T,T>) > second_less_then = [](const Pair & a, const Pair & b){
-        return a.second < b.second;
+    template<>
+    struct first_less_then<true>{
+        const bool operator () (const Point &a, const Point &b) const {
+            return a.y() < b.y();    
+        };
     };
 
-    template<
-        class RandomIt,
-        typename T 
-    >
+    template<class RandomIt, bool inverted = false> 
     void means_and_medians(
         RandomIt begin, 
         RandomIt end, 
         ListDigraph::Node father, 
         ListDigraph * clk_tree, 
-        ListDigraph::NodeMap< pair<T,T> > * node_to_pair,
-        const std::function< bool(pair<T,T>, pair<T,T>) > & comp = first_less_then<T> 
-    ){
+        ListDigraph::NodeMap< Point > * node_to_pair
+    )
+    {
         auto distance = std::distance(begin,end);
         if(distance == 0){
             return;
         }
 
-        auto region_mass_center = region_mean<Pair>(begin, end);
-        
+        auto region_mass_center = region_mean(begin, end);
         auto son = clk_tree->addNode();
         clk_tree->addArc(father, son);
         (*node_to_pair)[son] = region_mass_center; 
 
-        std::sort(begin, end, comp);
+        std::sort(begin, end, first_less_then<inverted>{});
 
         auto median = begin + distance / 2;
-        comp = comp == first_less_then<Pair> ? second_less_then<Pair> : first_less_then<Pair>;
-
-        means_and_medians(begin, median, son, clk_tree, node_to_pair, comp);
-        means_and_medians(median, end, son, clk_tree, node_to_pair, comp);
+        
+        means_and_medians<RandomIt,!inverted>(begin, median, son, clk_tree, node_to_pair);
+        means_and_medians<RandomIt,!inverted>(median+1, end, son, clk_tree, node_to_pair);
     }
 };
 
--- test/means_and_medians/mmm_test.cpp
+++ test/means_and_medians/mmm_test.cpp
@@ -2,17 +2,43 @@
 #include <mmm.h>
 #include <vector>
 #include <utility>
-#include <lemon/list_graph.h>
 
-TEST_CASE("too much m's in one algorithim", "[clk_router]"){
-    std::vector<std::pair<float,float>> v;
-    
+#include <lemon/list_graph.h>
+#include <ophidian/geometry/Models.h>
+
+using ophidian::geometry::Point;
+
+typedef lemon::ListDigraph::Node Node;
+typedef lemon::ListDigraph::Arc Arc;
+typedef lemon::ListDigraph::NodeIt Nodeit;
+typedef lemon::ListDigraph::ArcIt ArcIt;
+
+TEST_CASE("Routing one Point", "[clk_router][mmm]"){
+    std::vector<Point> v;
+   
+    v.push_back(Point{2,2});
+
     lemon::ListDigraph g;
     auto fountain = g.addNode();
-    lemon::ListDigraph::NodeMap<std::pair<float,float>> node_to_pin(g);
-    node_to_pin[fountain] = std::make_pair(0.0f,0.0f);
+    lemon::ListDigraph::NodeMap<Point> node_to_pin(g);
+    node_to_pin[fountain] = Point{0,0};
 
     clk_router::means_and_medians(std::begin(v), std::end(v), fountain, &g, &node_to_pin);
 
-    REQUIRE(0 == 0);
+    REQUIRE(node_to_pin[fountain].x() == 0.0 );
+    REQUIRE(node_to_pin[fountain].y() == 0.0 );
+
+    int count = 0;
+    for(ArcIt it(g); it != lemon::INVALID; ++it){
+        Arc arc(it);     
+        Node oppositeNode(g.oppositeNode(fountain, arc)) ;
+        REQUIRE(node_to_pin[oppositeNode].x() == 2.0 );
+        REQUIRE(node_to_pin[oppositeNode].y() == 2.0 );
+        count++;
+    }
+    REQUIRE(count ==1);
+}
+
+TEST_CASE("Routing many Points", "[clk_router][mmm]"){
+    
 }
